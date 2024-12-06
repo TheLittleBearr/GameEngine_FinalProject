5ddf8ea1c283278c757a9af282d2a457ea1a9a6e
Em Martin <emmartinns@gmail.com>
2024-12-06 14:53:54 -0400

    fixed water shader

--- Assets/quitGame.cs
+++ Assets/quitGame.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class quitGame : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void doExitGame()
     {
-        
-    }
+        Application.Quit();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("QUIT");
     }
 }
